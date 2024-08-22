b7145fa9e9d1e9932dd85ad881c91ea14be6dd89
Orlando Junior <orlandojsjunior@hotmail.com>
2024-08-21 22:18:31 -0300

    Update - Corrigindo o enunciado do exercício

--- Exercicio_2.2.py
+++ Exercicio_2.2.py
@@ -1,7 +1,9 @@
-# Exercício 2.2 	Digite a seguinte expressão no interpretador: 10 / 3 x 10² + 1 - 10 x 4 / 2
+'''
+Exercício 2.2 	Digite a seguinte expressão no interpretador: 10 / 3 x 10² + 1 - 10 x 4 / 2
 
-# Tente resolver o mesmo cálculo, usando apenas lápis e papel. Observe como a prioridade das operações é importante.
+Tente resolver o mesmo cálculo, usando apenas lápis e papel. Observe como a prioridade das operações é importante.
 
+'''
 Expressao = 10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2
 
 print(Expressao)
